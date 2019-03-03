5a190e67b21eaefaf80e2909dd9524b4cd5ce878
Lorenzo <lorenzo97@fastwebnet.it>
2019-03-03 19:01:29 +0100

    processing user input

--- plutonbot.py
+++ plutonbot.py
@@ -17,22 +17,34 @@ logger = logging.getLogger(__name__)
 
 MEMBERS, RECOMMENDATIONS = range(2)
 
+custom_keyboard = [['Rolenzo','Raffaele'], ['Zacco',"Endeavor"],["Ma D.","John Smith"],["Plutone","Tutti"]]
+reply_markup = ReplyKeyboardMarkup(custom_keyboard,one_time_keyboard=True)
+
 #every callback must feature bot and update as positional arguments
 def help(bot,update):
     update.message.reply_text('Hi, I\'m a bot. I will help you recommend stuff you like to your friends.\n' +
     'Here\'s a list of available commands\n '+
-    '/add - give an advice to a friend!\n'+    
-    '/remove - cancel a wrong recommendation. You don\'t want them to think you have shit taste!')
+    '/add - give an advice to a friend\n'+    
+    '/remove - cancel a wrong recommendation. You don\'t want them to think you have shit taste!'+
+    '/get - visualize the recommendations your friends made for you'    )
 
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 def add(bot, update):
-    custom_keyboard = [['Rolenzo','Raffaele'], ['Zacco',"Endeavor"],["Ma D.","John Smith"],["Plutone","Tutti"]]
-    reply_markup = ReplyKeyboardMarkup(custom_keyboard,one_time_keyboard=True)
-    chosen_member = bot.send_message(chat_id=update.message.chat_id, text="Choose the person you want to advise", reply_markup=reply_markup)
-    #return MEMBERS
+    bot.send_message(chat_id=update.message.chat_id, text="Choose the person you want to advise", reply_markup=reply_markup)
+    return MEMBERS
+
+def member(bot,update):
+    print(update.message.reply_text)
+    return ConversationHandler.END
+
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
 
 def main():
 
@@ -41,23 +53,20 @@ def main():
     updater.bot.setWebhook("https://advisorplutone.herokuapp.com/" + token)
 
 
-
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
 
     # need to handle keyboard response
-    #add_conv_handler = ConversationHandler(
-    #    entry_points=[CommandHandler('add', add)],
+    add_conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('add', add)],
 
-    #    states={
-    #        NAME : [MessageHandler(Filters.text, name)],
+        states={
+            MEMBERS : [MessageHandler(Filters.text, member)]
+        },
 
-    #        OBJECTS : [MessageHandler(Filters.text, ogg)]
-    #    },
-
-    #    fallbacks=[CommandHandler('cancel', cancel)]
-    #)
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
 
     
 
